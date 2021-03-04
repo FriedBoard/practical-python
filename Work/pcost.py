75498aaadf2b772fbff731513e6ba16fa7c2d156
Leroy van der Steenhoven <leroy.vandersteenhoven@student.hu.nl>
2021-03-04 14:01:14 +0100

    -

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+total_Cost = 0.00
+with open('Data/portfolio.csv', 'rt') as f:
+    header = next(f)
+    for line in f:
+        line = line.split(',')
+        total_Cost = total_Cost + (int(line[1]) * float(line[2]))
+    print('Total cost', total_Cost)
+    f.close()
