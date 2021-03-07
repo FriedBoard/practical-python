c4a31d23bf679955910fbfbc6ad7627415c98d0d
Leroy van der Steenhoven <leroy.vandersteenhoven@student.hu.nl>
2021-03-07 13:03:47 +0100

    2.2

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,65 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+def read_portfolio(filename):
+    '''Show what is in a portfolio and the total cost'''
+    portfolio = []
+    cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = {
+                    'name':row[0],
+                    'shares':int(row[1]),
+                    'price':float(row[2])
+                }
+            portfolio.append(holding)
+
+    # Calculate total cost
+    for holding in portfolio:
+        cost += holding['shares'] * holding['price']
+    return(portfolio, cost)
+
+def prices():
+    f = open('Data/prices.csv', 'r')
+    prices = {}
+    rows = csv.reader(f)
+    for row in rows:
+        try:
+            prices[row[0]] = row[1]
+        except:
+            continue
+    return(prices)
+
+def trade_report(prices, portfolio):
+    report = []
+    for holding in portfolio:
+        purchase_price = holding['shares'] * holding['price']
+        value_change = holding['shares'] * float(prices[holding['name']]) - purchase_price
+        report.append({
+            'name':holding['name'],
+            'shares':holding['shares'],
+            'current price':prices[holding['name']],
+            'purchase price':holding['price'],
+            'value change':round(float(value_change), 2),
+        })
+    return(report)
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+
+
+total_portfolio, total_cost = read_portfolio(filename)
+prices = prices()
+trade_report = trade_report(prices, total_portfolio)
+
+
+print('Portfolio holdings:', total_portfolio, '\nTotal cost was:', total_cost)
+print(trade_report)
--- Work/scratch 2.2.py
+++ Work/scratch 2.2.py
@@ -20,3 +20,32 @@ print(prices)
 # Get
 price = prices.get('"AXP"', 0.0)
 print(price)
+
+# Composite keys
+holidays = {
+    (1, 1) : 'New Years',
+    (3, 14) : 'Pi day',
+    (9, 13) : "Programmer' s day",
+}
+
+print(holidays[1, 1])
+
+# Sets
+tech_stocks = set(['IBM','AAPL','MSFT'])
+if 'IBM' in tech_stocks:
+    print('IBM is in', 'tech stocsk')
+
+names = ['IBM', 'IBM', 'AAPL','MSFT']
+names = set(names) # duplicate elimination
+print(names)
+names.add('CSCO')
+names.remove('IBM')
+print(names)
+
+# Combine sets
+names_1 = set(['IBM', 'AAPL'])
+names_2 = set(['CSCO','MSFT'])
+names_union = names_1 | names_2
+print(names_union)
+names_intersection = names_1 & names_2
+print(names_intersection)
