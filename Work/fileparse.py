80c29a39d6678ae20c7e06adc1810532280ba56f
Leroy van der Steenhoven <leroy.vandersteenhoven@student.hu.nl>
2021-03-10 12:27:57 +0100

    3.3

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,10 +3,15 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename='Data/portfolio.csv', select=['name', 'shares', 'price'], types=[str, int, float], has_headers=True, delimiter=','):
+def parse_csv(filename='Data/portfolio.csv', select=['name', 'shares', 'price'], types=[str, int, float], has_headers=True, delimiter=',', silence_errors=True):
     '''
     Parse a CSV to a list of records
     '''
+
+    # Check if select and headers false aren't used together
+    if select and not has_headers:
+        raise RuntimeError('select argument requires column headers')
+
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
 
@@ -25,19 +30,26 @@ def parse_csv(filename='Data/portfolio.csv', select=['name', 'shares', 'price'],
             indices = []
         
         records = []
-        for row in rows:
+        for rowno, row in enumerate(rows, start=1):
             if not row: # skip empty rows
                 continue
             
             if indices:
                 row = [row[index] for index in indices]
             
-            if types:
-                row = [func(val) for func, val in zip(types, row)]
+            try:
+                if types:
+                    row = [func(val) for func, val in zip(types, row)]
 
-            if has_headers:
-                record = dict(zip(headers, row))
-            else:
-                record = row
-            records.append(record)
+                if has_headers:
+                    record = dict(zip(headers, row))
+                else:
+                    record = row
+                records.append(record)
+            except ValueError as e:
+                if silence_errors:
+                    continue
+                else:
+                    print('row {}, {} has problems'.format(rowno, row))
+                    print('row {} problem: {}'.format(rowno, e))
     return(records)
